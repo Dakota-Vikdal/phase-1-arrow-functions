92b7d30317cf0b22c3c2ab472bb20576b7454147
Dakota Vikdal <dakota.james19@hotmail.com>
2023-01-11 17:14:43 -0500

    Arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide() {
+    return 2000/100
+}
+
+const square = (parameter) => {
+    return parameter * parameter
+}
+
+const add = (parameter, parameter2) => {
+    return parameter + parameter2
+}
